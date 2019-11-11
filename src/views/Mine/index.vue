61b58ebd9d57cadf8994faff0df424de14685a43
norma-jiang <1661586220@qq.com>
2019-11-12 01:46:51 +0800

    add createComponents

---
+++ src/views/Mine/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="content">
+        <Login/>
+    </div>
+</template>
+
+<script>
+    import Login from "@/components/Login"
+    export default {
+        name: "Mine",
+        components:{
+            Login
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
